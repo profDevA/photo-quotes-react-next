28fd13de097ab0a0b3d20752680d30cc8f45f557
comet1112 <sunearth.moon@outlook.com>
2020-11-02 16:46:36 +0700

    add animate in react

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
-import '../styles/lib/grid.css'
+import {useState, useEffect} from 'react'
 import '../styles/fonts.scss'
-import 'animate.css'
+import '../styles/lib/grid.css'
+import 'animate.css/animate.min.css'
 import '../styles/style.scss'
+const isServer = typeof window === 'undefined'
+const WOW = !isServer ? require('wow.js') : null
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    new WOW().init();
+  }, []);
   return <Component {...pageProps} />
 }
 
