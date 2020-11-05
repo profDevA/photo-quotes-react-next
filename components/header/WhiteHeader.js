d6546639150ce81c391c64f1e24f84d0c48a9af5
comet1112 <sunearth.moon@outlook.com>
2020-11-05 20:16:06 +0700

    change white head

--- components/header/WhiteHeader.js
+++ components/header/WhiteHeader.js
@@ -5,29 +5,36 @@ import Image from 'next/image'
 
 export default function WhiteHeader() {
     return (
-        <header className="container-fluid header__transparent">
-            <div className="container">
-                <div className="top-header__white top-header">
-                    <div className="top-header__left">
-                        <a href="/">
-                            <Image
-                                src="/assets/logo-white.png"
-                                alt="Logo Image"
-                                unsized
-                            />
-                        </a>
-                    </div>
-                    <Menu />
-                    <div className="top-header__right">
-                        <a href=""><span className="icon-user"></span></a>
-                        <div className="menu-icon">
-                            <span></span>
-                            <span></span>
+        <>
+            <Head>
+                <meta name="robots" content="noindex" />
+                <title>Photo Quotes</title>
+                <link rel="icon" href="/favicon.png" />
+            </Head>
+            <header className="container-fluid header__transparent">
+                <div className="container">
+                    <div className="top-header__white top-header">
+                        <div className="top-header__left">
+                            <a href="/">
+                                <Image
+                                    src="/assets/logo-white.png"
+                                    alt="Logo Image"
+                                    unsized
+                                />
+                            </a>
+                        </div>
+                        <Menu />
+                        <div className="top-header__right">
+                            <a href=""><span className="icon-user"></span></a>
+                            <div className="menu-icon">
+                                <span></span>
+                                <span></span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <Filter />
-        </header>
+                <Filter />
+            </header>
+        </>
     )
 }
