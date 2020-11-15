2bcd07cb154404931d3848655e00d32023fd6ef2
comet1112 <sunearth.moon@outlook.com>
2020-11-15 23:35:09 +0700

    change server

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
         DEV_SERVER_URI: 'http://127.0.0.1:8000',
-        PROD_SERVER_URI: 'https://jairo.mycouncil.ie/',
+        PROD_SERVER_URI: 'https://thawing-brook-78067.herokuapp.com',
     },
 }
